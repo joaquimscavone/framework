5e2aab360ced19e35e8188bbc0284faec23d787e
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2024-11-02 14:38:42 -0300

    Criando Model (aula 31-10)

--- Database/DB.php
+++ Database/DB.php
@@ -35,15 +35,22 @@ class DB{
         return new $driver($this->parameters); 
     }
 
-    public static function query($table_name){
+    public static function connection($connection_name = null){
+
         $database_file = defined('DATABASES_CONFIG_FILE') ? constant('DATABASES_CONFIG_FILE') : 'database';
-        $conneciton_name = Config::getConfig("$database_file.connection_default");
-        $conneciton_name = defined('DATABASE_CONNECTION_DEFAULT') ? constant('DATABASE_CONNECTION_DEFAULT') : $conneciton_name;
-        if(is_null($conneciton_name)){
+
+        $connection_name = $connection_name ?? Config::getConfig("$database_file.connection_default");
+        $connection_name = $connection_name ?? ((defined('DATABASE_CONNECTION_DEFAULT')) ? constant('DATABASE_CONNECTION_DEFAULT') : null);
+        
+        if(is_null($connection_name)){
             throw new Exception("Para criar uma query diretamente você precisa definir uma conexõa padrão, para isso 
                                     defina a constante DATABASE_CONNECTION_DEFAULT com o nome da conexão ou 
                                     crie um key com o nome connection_default dentro do arquivo $database_file de configuração");
         }
-        return (new DB($conneciton_name))->table($table_name);
+        return (new DB($connection_name));
+    }
+
+    public static function query($table){
+        return static::connection()->table($table);
     }
 }
